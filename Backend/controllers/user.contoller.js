fc742fc7ce51d87c446541847f032039a94f4999
Abhishek Bharti <abhibharti457@gmail.com>
2025-11-30 00:17:08 +0530

    add signup controller with email validation and user creation

---
+++ Backend/controllers/user.contoller.js
@@ -0,0 +1,23 @@
+import User from "../models/user.model";
+
+export const signup = async (req,res) =>{
+    const {firstname,lastname,email,password} = req.body;
+    try{
+      
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        return res.status(400).json({message:"User already exists"});
+    }
+    const newUser = new User({
+        firstname,
+        lastname,
+        email,
+        password
+    });
+    await newUser.save();
+    res.status(201).json({message:"User created successfully", user:newUser});
+    }catch(error){
+        console.error("error", error);
+        res.status(500).json({message:"Server Error"});
+    } 
+};
