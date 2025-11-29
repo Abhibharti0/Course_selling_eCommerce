26bbebf06dc463b347c9b769e0d599450638a383
Abhishek Bharti <abhibharti457@gmail.com>
2025-11-29 23:40:19 +0530

    code for deleteCourse

--- Backend/controllers/course.controller.js
+++ Backend/controllers/course.controller.js
@@ -91,3 +91,18 @@ export const updateCourse = async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 };
+
+
+export const deleteCourse = async (req, res) => {
+  const { id } = req.params;    
+  try {
+    const course = await Course.findByIdAndDelete(id);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }     
+    res.status(200).json({ message: "Course deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
--- Backend/routes/course.route.js
+++ Backend/routes/course.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { createCourse,updateCourse } from '../controllers/course.controller.js';
+import { createCourse,updateCourse,deleteCourse } from '../controllers/course.controller.js';
 
 const router = express.Router();
 
 
 router.post('/create', createCourse);
 router.put('/update/:id', updateCourse);
-    
+router.delete('/delete/:id', deleteCourse);  
 
 export default router;
