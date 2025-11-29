a774c82013c5d31880e62c56d458a6b3c16288e4
Abhishek Bharti <abhibharti457@gmail.com>
2025-11-30 02:01:18 +0530

    code for login user

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -42,3 +42,24 @@ export const signup = async (req,res) =>{
         res.status(500).json({message:"Server Error"});
     } 
 };
+
+
+export const login = async (req,res) =>{
+    const {email,password} = req.body;
+
+    try{
+         const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        } 
+        res.status(200).json({message:"Login successful", user});
+} catch(error){
+    res.status(500).json({message:"Error in login"});
+    console.log("error", error);
+}
+}
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { signup } from '../controllers/user.controller.js';
+import { signup,login } from '../controllers/user.controller.js';
 
 
 const router = express.Router();
 
 
 router.post('/signup', signup);
+router.post('/login', login);
 
 
 
