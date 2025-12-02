736f22173a8bb3a7b3180b1c5eab6278b3473370
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-02 15:48:36 +0530

    UI for AllCourses

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,17 +3,24 @@ import Home from './components/Home.jsx'
 import Login from './components/Login.jsx'
 import Signup from './components/Signup.jsx'
 import { Toaster } from 'react-hot-toast'
+import Courses from './components/Courses.jsx'
+import Buy from './components/Buy.jsx'
+import Purchases from './components/Purchases.jsx'
 
 function App() {
   return (
     <>
+      <Toaster />
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-      </Routes>
 
-      <Toaster />
+        <Route path='/courses' element={<Courses />} />
+        <Route path='/buy/:courseId' element={<Buy />} />
+        <Route path='/purchases' element={<Purchases />} />
+      </Routes>
     </>
   )
 }
