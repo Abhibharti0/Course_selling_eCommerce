222231ef6585f39f3ba743f2c4a7d5d9d1ef4c5e
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-05 00:09:40 +0530

    fix

--- Backend/config.js
+++ Backend/config.js
@@ -6,7 +6,7 @@ dotenv.config();
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD || 'user123';
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD || 'user123';
 
-const STRIPE_SECRET_KEY="sk_test_51SaapsB2Xb6LSmp3MP2mdVfQL3EbtTcs0PHfjCA2HxA9pz5fzFoxvtiYBHicrDAd5O694FE1RHD075kK3Vlb7Lof003vlXmLOf"
+const STRIPE_SECRET_KEY=process.env.STRIPE_SECRET_KEY
 
 export default {
   JWT_USER_PASSWORD,
