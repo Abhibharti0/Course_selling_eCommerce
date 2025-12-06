e4b30c84d8fdfac47b9ad0f5e045ec2725cfdf53
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-06 13:36:32 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -16,6 +16,18 @@ import cors from 'cors';
 const app = express();
 dotenv.config();
 
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Set-Cookie"]
+  })
+);
+
+
 app.use(cookieParser());
 
 app.use(express.json());
@@ -27,15 +39,7 @@ app.use(fileUpload({
   tempFileDir: '/tmp/',
 }));
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
-    exposedHeaders: ["Set-Cookie"]
-  })
-);
+
 
 
 const PORT = process.env.PORT || 5000;
