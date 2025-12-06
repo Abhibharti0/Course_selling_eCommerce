ff222342d874f9daada34ec4cb038198613e2e3e
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-06 12:36:48 +0530

    fixes

--- Backend/index.js
+++ Backend/index.js
@@ -27,13 +27,16 @@ app.use(fileUpload({
   tempFileDir: '/tmp/',
 }));
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
- 
-}))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+    exposedHeaders: ["Set-Cookie"]
+  })
+);
+
 
 const PORT = process.env.PORT || 5000;
 const mongoUrl = process.env.MONGO_URL;
